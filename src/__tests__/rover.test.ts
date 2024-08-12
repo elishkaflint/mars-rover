50ad32688a6645806f6954c09e776f30e41d93df
Elishka Flint <elishkaf@gmail.com>
2024-08-12 15:15:05 +0100

    Lost rovers returns last position and orientation

--- src/__tests__/rover.test.ts
+++ src/__tests__/rover.test.ts
@@ -4,13 +4,16 @@ import {Rover} from "../rover";
 describe('Rover', () => {
     describe('Given 1 x 1 grid', () => {
         describe('When facing north and moves forwards', () => {
-            it('Becomes lost', () => {
+            it('Becomes lost and returns last position', () => {
                 const grid: Grid = new Grid(1,1);
                 const rover: Rover = new Rover(0, 0, 'N', grid);
 
                 rover.moveForward();
 
                 expect(rover.isLost).toBe(true);
+                expect(rover.x).toBe(0);
+                expect(rover.y).toBe(0);
+                expect(rover.orientation).toBe('N');
             })
         })
     })
