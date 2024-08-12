d479aed86e44b127fcd7d31ce9a52810cc835988
Elishka Flint <elishkaf@gmail.com>
2024-08-12 15:17:32 +0100

    Rover gets lost in all directions

--- src/__tests__/rover.test.ts
+++ src/__tests__/rover.test.ts
@@ -16,5 +16,44 @@ describe('Rover', () => {
                 expect(rover.orientation).toBe('N');
             })
         })
+        describe('When facing east and moves forwards', () => {
+            it('Becomes lost and returns last position', () => {
+                const grid: Grid = new Grid(1,1);
+                const rover: Rover = new Rover(0, 0, 'E', grid);
+
+                rover.moveForward();
+
+                expect(rover.isLost).toBe(true);
+                expect(rover.x).toBe(0);
+                expect(rover.y).toBe(0);
+                expect(rover.orientation).toBe('E');
+            })
+        })
+        describe('When facing south and moves forwards', () => {
+            it('Becomes lost and returns last position', () => {
+                const grid: Grid = new Grid(1,1);
+                const rover: Rover = new Rover(0, 0, 'S', grid);
+
+                rover.moveForward();
+
+                expect(rover.isLost).toBe(true);
+                expect(rover.x).toBe(0);
+                expect(rover.y).toBe(0);
+                expect(rover.orientation).toBe('S');
+            })
+        })
+        describe('When facing west and moves forwards', () => {
+            it('Becomes lost and returns last position', () => {
+                const grid: Grid = new Grid(1,1);
+                const rover: Rover = new Rover(0, 0, 'W', grid);
+
+                rover.moveForward();
+
+                expect(rover.isLost).toBe(true);
+                expect(rover.x).toBe(0);
+                expect(rover.y).toBe(0);
+                expect(rover.orientation).toBe('W');
+            })
+        })
     })
 })
