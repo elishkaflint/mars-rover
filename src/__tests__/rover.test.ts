09274d57c80d0c1760ca6b627b8aafd9af78d5f5
Elishka Flint <elishkaf@gmail.com>
2024-08-12 17:26:43 +0100

    Another test refactor

--- src/__tests__/rover.test.ts
+++ src/__tests__/rover.test.ts
@@ -4,9 +4,10 @@ import { assertExpected } from './utils';
 
 describe('Rover unit tests', () => {
 
-    describe('When facing north', () => {
-        test('Moves forwards', () => {
-            const grid = new Grid(3, 3);
+    const grid = new Grid(3, 3);
+
+    describe('Moves forwards', () => {
+        test('When facing north', () => {
             const rover = new Rover(1, 1, 'N', grid);
 
             rover.moveForward();
@@ -17,9 +18,47 @@ describe('Rover unit tests', () => {
                 y: 2,
                 orientation: 'N',
             });
-        });
-        test('Turns left', () => {
-            const grid = new Grid(3, 3);
+        })
+        test('When facing east', () => {
+            const rover = new Rover(1, 1, 'E', grid);
+
+            rover.moveForward();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 2,
+                y: 1,
+                orientation: 'E',
+            });
+        })
+        test('When facing south', () => {
+            const rover = new Rover(1, 1, 'S', grid);
+
+            rover.moveForward();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 0,
+                orientation: 'S',
+            });
+        })
+        test('When facing west', () => {
+            const rover = new Rover(1, 1, 'W', grid);
+
+            rover.moveForward();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 0,
+                y: 1,
+                orientation: 'W',
+            });
+        })
+    })
+
+    describe('Turns left', () => {
+        test('When facing north', () => {
             const rover = new Rover(1, 1, 'N', grid);
 
             rover.turnLeft();
@@ -30,9 +69,47 @@ describe('Rover unit tests', () => {
                 y: 1,
                 orientation: 'W',
             });
-        });
-        test('Turns right', () => {
-            const grid = new Grid(3, 3);
+        })
+        test('When facing east', () => {
+            const rover = new Rover(1, 1, 'E', grid);
+
+            rover.turnLeft();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'N',
+            });
+        })
+        test('When facing south', () => {
+            const rover = new Rover(1, 1, 'S', grid);
+
+            rover.turnLeft();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'E',
+            });
+        })
+        test('When facing west', () => {
+            const rover = new Rover(1, 1, 'W', grid);
+
+            rover.turnLeft();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'S',
+            });
+        })
+    })
+
+    describe('Turns right', () => {
+        test('When facing north', () => {
             const rover = new Rover(1, 1, 'N', grid);
 
             rover.turnRight();
@@ -43,9 +120,47 @@ describe('Rover unit tests', () => {
                 y: 1,
                 orientation: 'E',
             });
-        });
-        test('Gets lost', () => {
-            const grid: Grid = new Grid(3, 3);
+        })
+        test('When facing east', () => {
+            const rover = new Rover(1, 1, 'E', grid);
+
+            rover.turnRight();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'S',
+            });
+        })
+        test('When facing south', () => {
+            const rover = new Rover(1, 1, 'S', grid);
+
+            rover.turnRight();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'W',
+            });
+        })
+        test('When facing west', () => {
+            const rover = new Rover(1, 1, 'W', grid);
+
+            rover.turnRight();
+
+            assertExpected(rover, {
+                isLost: false,
+                x: 1,
+                y: 1,
+                orientation: 'N',
+            });
+        })
+    })
+
+    describe('Gets lost', () => {
+        test('When facing north', () => {
             const rover: Rover = new Rover(1, 2, 'N', grid);
 
             rover.moveForward();
@@ -56,51 +171,8 @@ describe('Rover unit tests', () => {
                 y: 2,
                 orientation: 'N',
             });
-        });
-    });
-
-    describe('When facing east', () => {
-        test('Moves forwards', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'E', grid);
-
-            rover.moveForward();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 2,
-                y: 1,
-                orientation: 'E',
-            });
-        });
-        test('Turns left', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'E', grid);
-
-            rover.turnLeft();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'N',
-            });
-        });
-        test('Turns right', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'E', grid);
-
-            rover.turnRight();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'S',
-            });
-        });
-        test('Gets lost', () => {
-            const grid: Grid = new Grid(3, 3);
+        })
+        test('When facing east', () => {
             const rover: Rover = new Rover(2, 1, 'E', grid);
 
             rover.moveForward();
@@ -111,51 +183,8 @@ describe('Rover unit tests', () => {
                 y: 1,
                 orientation: 'E',
             });
-        });
-    });
-
-    describe('When facing south', () => {
-        test('Moves forwards', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'S', grid);
-
-            rover.moveForward();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 0,
-                orientation: 'S',
-            });
-        });
-        test('Turns left', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'S', grid);
-
-            rover.turnLeft();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'E',
-            });
-        });
-        test('Turns right', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'S', grid);
-
-            rover.turnRight();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'W',
-            });
-        });
-        test('Gets lost', () => {
-            const grid: Grid = new Grid(3, 3);
+        })
+        test('When facing south', () => {
             const rover: Rover = new Rover(1, 0, 'S', grid);
 
             rover.moveForward();
@@ -166,51 +195,8 @@ describe('Rover unit tests', () => {
                 y: 0,
                 orientation: 'S',
             });
-        });
-    });
-
-    describe('When facing west', () => {
-        test('Moves forwards', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'W', grid);
-
-            rover.moveForward();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 0,
-                y: 1,
-                orientation: 'W',
-            });
-        });
-        test('Turns left', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'W', grid);
-
-            rover.turnLeft();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'S',
-            });
-        });
-        test('Turns right', () => {
-            const grid = new Grid(3, 3);
-            const rover = new Rover(1, 1, 'W', grid);
-
-            rover.turnRight();
-
-            assertExpected(rover, {
-                isLost: false,
-                x: 1,
-                y: 1,
-                orientation: 'N',
-            });
-        });
-        test('Gets lost', () => {
-            const grid: Grid = new Grid(3, 3);
+        })
+        test('When facing west', () => {
             const rover: Rover = new Rover(0, 1, 'W', grid);
 
             rover.moveForward();
@@ -220,6 +206,6 @@ describe('Rover unit tests', () => {
                 y: 1,
                 orientation: 'W',
             });
-        });
-    });
+        })
+    })
 });
